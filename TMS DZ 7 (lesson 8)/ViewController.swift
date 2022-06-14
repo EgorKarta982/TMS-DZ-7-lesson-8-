78dbae77bee7941b59273c7ccba234ca07d57ea5
Egor Kartshev <egorkarta982@gmail.com>
2022-06-15 02:30:32 +0300

    Initial Commit

---
+++ TMS DZ 7 (lesson 8)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TMS DZ 7 (lesson 8)
+//
+//  Created by Егор on 15.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
